f44430870229737ae00313afa086b97de8697d84
Muhammed Kans <kanskabeer@gmail.com>
2026-05-09 09:31:40 +0530

    map() ,  filter() , forEach(),

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -9,3 +9,39 @@ const array2 =  array1.filter((item)=>{
 })
 
 console.log(array2)
+
+
+
+const products = ["iPhone 15", "Samsung S23", "MacBook Air", "Google Pixel"];
+
+const searchText = "Phone"; 
+
+const searchResults =  products.filter((product)=>{
+  return product.toLowerCase().includes(searchText.toLowerCase())
+})
+
+
+console.log(searchResults)
+
+
+
+
+const array = ["pending" , "shipping", "delivering", "pending"];
+
+
+        const  reduce =  array.reduce((acc,status)=>{
+              acc[status] = (acc[status] || 0)+1
+              return acc
+        },{})
+
+        console.log(reduce);
+
+
+
+
+
+        const users = ["Azna", "Rahul", "Sneha"];
+users.forEach((user) => {
+    console.log(`Sending email to ${user}... ✅`);
+    // Logic to send email would go here
+});
